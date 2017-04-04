f5488881149014a697ac38f822cd1a12e52cb1ab
trzl47 <brownty47@gmail.com>
2017-04-04 02:29:02 -0500

    simple Banner component

---
+++ src/components/Banner.js
@@ -0,0 +1,16 @@
+// Libs
+import React from 'react';
+// Components
+
+const Banner = () => {
+		return (
+			<div className={["feature"].join(' ')}>
+				<div className={["container"].join(' ')}>
+					<h1>Available everywhere</h1>
+					<p>Start watching on one device, and pick up where you left off on another device.</p>
+				</div>
+			</div>
+		);
+	}
+
+export default Banner;
