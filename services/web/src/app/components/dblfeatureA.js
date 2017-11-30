b0d290dff8e1916c605d383f2db57bdb8c74eef8
trzl47 <brownty47@gmail.com>
2017-11-30 15:53:42 -0600

    v1 image built

--- services/web/src/app/components/dblfeatureA.js
+++ services/web/src/app/components/dblfeatureA.js
@@ -3,10 +3,10 @@ import React from 'react';
 const DblfeatureA = props => (
   <div className="row">
 		<div className="col-md-8">
-			<img className="thumbnail" alt='' role='presentation' src={props.img1} />
+			<img className="thumbnail" alt='' src={props.img1} />
 			</div>
 		<div className="col-md-4">
-			<img className="thumbnail" alt='' role='presentation' src={props.img2} />
+			<img className="thumbnail" alt='' src={props.img2} />
 		</div>
   </div>
 );
